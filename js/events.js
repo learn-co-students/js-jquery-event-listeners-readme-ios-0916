dbfb6f8b22a694dba98973360277914c20cb3817
Johann Kerr <johannkerr@gmail.com>
2017-03-30 02:14:41 -0400

    Done.

--- js/events.js
+++ js/events.js
@@ -1,7 +1,34 @@
 //define functions here
+function pressIt() {
+  $("input:first").on("keydown", function(e) {
+    if (e.which === 71) {
+      alert("You pressed 'g'!")
+    }
+  })
+}
+
+function submitIt() {
+  $("form").on("submit", function(e) {
+    alert("Your form is going to be submitted now.")
+  })
+}
+
+
+
+
+function getIt() {
+  $("p").on("click", function() {
+    alert("Hey!")
+  })
+}
+
+function frameIt() {
+  $('img').on('load', function(){
+    $('img').addClass('tasty')
+  })
+}
 
 $(document).ready(function(){
-
 // call functions here
 
 });
